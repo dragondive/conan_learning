9b5a3640f925c7640b80c38cb6337c0783f8839e
Aravind Pai <dragondive@outlook.in>
2020-12-05 14:14:22 +0530

    Added conanfile.py to create the package

---
+++ conan_gtest/test_package/example.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "sample.h"
+
+int main() {
+    std::cout << get_hello() << " " << get_42() << std::endl;
+}
---
+++ conan_gtest_use/conan_gtest_use.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "sample.h"
+
+int main()
+{
+    std::cout << get_hello() << " " << get_42() << std::endl;
+}
